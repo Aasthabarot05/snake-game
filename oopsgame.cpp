a84a341709a6376db7f5eee626e405ae89a7ab58
Aasthabarot05 <barotaastha2006@gmail.com>
2025-02-12 13:03:20 +0530

    Update oopsgame.cpp

--- oopsgame.cpp
+++ oopsgame.cpp
@@ -1,127 +1,23 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
-
 using namespace std;
 
-
-const int width = 40;
-const int height = 20;
-
-enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
-
-class Food {
-public:
-    int x, y;
-
-    Food() {
-        Respawn();
-    }
-
-    void Respawn() {
-        x = rand() % width;
-        y = rand() % height;
-    }
-};
-
-class Snake {
+class SnakeGame {
 private:
-    int x, y;
-    int TailX[100], TailY[100];
+    bool gameover;
+    bool gamewin;
+    const int width;
+    const int height;
+    int x, y, fruitY, score, fruitX;
+    int TailX[100];
+    int TailY[100];
     int ntail;
+    const int maxTailLength;
+    bool paused;
+
+    enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
     eDirection dir;
-
-public:
-    Snake() {
-        Reset();
-    }
-
-    void Reset() {
-        x = width / 2;
-        y = height / 2;
-        ntail = 0;
-        dir = STOP;
-    }
-
-    void Move() {
-        if (dir == STOP) return; 
-
-
-        int prevX = TailX[0];
-        int prevY = TailY[0];
-        int prev2X, prev2Y;
-        TailX[0] = x;
-        TailY[0] = y;
-
-        for (int i = 1; i < ntail; i++) {
-            prev2X = TailX[i];
-            prev2Y = TailY[i];
-            TailX[i] = prevX;
-            TailY[i] = prevY;
-            prevX = prev2X;
-            prevY = prev2Y;
-        }
-
-       
-        switch (dir) {
-            case LEFT:  x--; break;
-            case RIGHT: x++; break;
-            case UP:    y--; break;
-            case DOWN:  y++; break;
-            default: break;
-        }
-    }
-
-    bool CheckCollision() {
-      
-        if (x < 0 || x >= width || y < 0 || y >= height) return true;
-
-        for (int i = 0; i < ntail; i++) {
-            if (TailX[i] == x && TailY[i] == y) return true;
-        }
-        return false;
-    }
-
-    void Grow() {
-        ntail++;
-    }
-
-    void SetDirection(eDirection newDir) {
-        if (dir == STOP) { 
-            dir = newDir;
-            return;
-        }
-        
-   
-        if ((dir == LEFT && newDir != RIGHT) ||
-            (dir == RIGHT && newDir != LEFT) ||
-            (dir == UP && newDir != DOWN) ||
-            (dir == DOWN && newDir != UP)) {
-            dir = newDir;
-        }
-    }
-
-    int GetX() { return x; }
-    int GetY() { return y; }
-    int GetTailLength() { return ntail; }
-    int GetTailX(int index) { return TailX[index]; }
-    int GetTailY(int index) { return TailY[index]; }
-};
-
-
-class Game {
-private:
-    bool gameover, paused;
-    Snake snake;
-    Food food;
-    int score;
-
-public:
-    Game() {
-        gameover = false;
-        paused = false;
-        score = 0;
-    }
 
     void PlaySoundEffects(int frequency, int duration) {
         Beep(frequency, duration);
@@ -135,10 +31,25 @@ public:
         cout << "Press ENTER to Start...\n";
         PlaySoundEffects(500, 300);
         while (true) {
-            if (_kbhit() && _getch() == 13) {
+            if (_kbhit() && _getch() == 13) { 
                 break;
             }
         }
+    }
+
+    void Setup() {
+        gameover = false;
+        gamewin = false;
+        dir = STOP;
+        x = width / 2;
+        y = height / 2;
+        score = 0;
+        ntail = 0;
+
+        do {
+            fruitX = rand() % width;
+            fruitY = rand() % height;
+        } while (fruitX == x && fruitY == y);
     }
 
     void SetCursorPosition(int x, int y) {
@@ -149,36 +60,43 @@ public:
     }
 
     void Draw() {
-        SetCursorPosition(0, 0);
-        
-        for (int i = 0; i < width + 2; i++) cout << "#";
+        for (int i = 0; i < width + 2; i++) {
+            cout << "#";
+        }
         cout << endl;
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (j == 0) cout << "#";
-                if (i == snake.GetY() && j == snake.GetX()) cout << "O";
-                else if (i == food.y && j == food.x) cout << "F";
+                if (j == 0) cout << "#"; 
+                if (i == y && j == x) cout << "O"; 
+                else if (i == fruitY && j == fruitX) cout << "F"; 
                 else {
                     bool print = false;
-                    for (int k = 0; k < snake.GetTailLength(); k++) {
-                        if (snake.GetTailX(k) == j && snake.GetTailY(k) == i) {
+                    for (int k = 0; k < ntail; k++) {
+                        if (TailX[k] == j && TailY[k] == i) {
                             cout << "o";
                             print = true;
                         }
                     }
                     if (!print) cout << " ";
                 }
-                if (j == width - 1) cout << "#";
+                if (j == width - 1) cout << "#"; 
             }
             cout << endl;
         }
 
-        for (int i = 0; i < width + 2; i++) cout << "#";
+        for (int i = 0; i < width + 2; i++) {
+            cout << "#";
+        }
         cout << endl;
 
         cout << "Score: " << score << endl;
-        if (paused) cout << "Game Paused. Press any key to resume...\n";
+        if (paused) {
+            cout << "Game Paused. Press any key to resume...\n";
+        }
+        if (gamewin) {
+            cout << "You Win! Press 'R' to Restart or 'Q' to Quit...\n";
+        }
     }
 
     void Input() {
@@ -188,32 +106,85 @@ public:
                 paused = false;
             } else {
                 switch (key) {
-                    case 'a': snake.SetDirection(LEFT); break;
-                    case 'd': snake.SetDirection(RIGHT); break;
-                    case 'w': snake.SetDirection(UP); break;
-                    case 's': snake.SetDirection(DOWN); break;
-                    case 'x': gameover = true; break;
-                    default: paused = true; break;
+                case 'a':
+                    if (dir != RIGHT) 
+                        dir = LEFT;
+                    break;
+                case 'd':
+                    if (dir != LEFT) 
+                        dir = RIGHT;
+                    break;
+                case 'w':
+                    if (dir != DOWN) 
+                        dir = UP;
+                    break;
+                case 's':
+                    if (dir != UP) 
+                        dir = DOWN;
+                    break;
+                case 'x':
+                    gameover = true;
+                    break;
+                default:
+                    paused = true;
+                    break;
                 }
             }
         }
     }
 
     void Logic() {
-        if (paused) return;
+        if (paused || gamewin) return;
 
-        snake.Move();
+        int prevX = TailX[0];
+        int prevY = TailY[0];
+        int prev2X, prev2Y;
+        TailX[0] = x;
+        TailY[0] = y;
+        for (int i = 1; i < ntail; i++) {
+            prev2X = TailX[i];
+            prev2Y = TailY[i];
+            TailX[i] = prevX;
+            TailY[i] = prevY;
+            prevX = prev2X;
+            prevY = prev2Y;
+        }
+        switch (dir) {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        default:
+            break;
+        }
 
-        if (snake.CheckCollision()) {
+        if (x < 0 || x >= width || y < 0 || y >= height) {
             gameover = true;
             return;
         }
 
-        if (snake.GetX() == food.x && snake.GetY() == food.y) {
+        for (int i = 0; i < ntail; i++) {
+            if (TailX[i] == x && TailY[i] == y)
+                gameover = true;
+        }
+
+        if (x == fruitX && y == fruitY) {
             score += 10;
-            food.Respawn();
-            snake.Grow();
+            fruitX = rand() % width;
+            fruitY = rand() % height;
+            ntail++;
             PlaySoundEffects(600, 150);
+            if (ntail == maxTailLength) {
+                gamewin = true;
+            }
         }
     }
 
@@ -229,7 +200,28 @@ public:
             if (_kbhit()) {
                 char key = _getch();
                 if (key == 'r' || key == 'R') {
-                    Reset();
+                    Setup();
+                    break;
+                } else if (key == 'q' || key == 'Q') {
+                    exit(0);
+                }
+            }
+        }
+    }
+
+    void ShowGameWinScreen() {
+        system("cls");
+        cout << "#############################\n";
+        cout << "#        YOU WIN!          #\n";
+        cout << "#############################\n";
+        cout << "Final Score: " << score << "\n";
+        cout << "Press 'R' to Restart or 'Q' to Quit...\n";
+        PlaySoundEffects(700, 500);
+        while (true) {
+            if (_kbhit()) {
+                char key = _getch();
+                if (key == 'r' || key == 'R') {
+                    Setup();
                     break;
                 } else if (key == 'q' || key == 'Q') {
                     exit(0);
@@ -242,37 +234,36 @@ public:
         HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
         CONSOLE_CURSOR_INFO cursorInfo;
         GetConsoleCursorInfo(hConsole, &cursorInfo);
-        cursorInfo.bVisible = FALSE;
+        cursorInfo.bVisible = FALSE; 
         SetConsoleCursorInfo(hConsole, &cursorInfo);
     }
 
-    void Reset() {
-        gameover = false;
-        paused = false;
-        score = 0;
-        snake.Reset();
-        food.Respawn();
-    }
+public:
+    SnakeGame() : width(40), height(20), maxTailLength(100) {}
 
     void Run() {
-        HideCursor();
+        HideCursor(); 
         ShowStartScreen();
-        Reset();
+        Setup();
         while (true) {
-            while (!gameover) {
+            while (!gameover && !gamewin) {
+                SetCursorPosition(0, 0); 
                 Draw();
                 Input();
                 Logic();
                 Sleep(200);
             }
-            ShowGameOverScreen();
+            if (gameover) {
+                ShowGameOverScreen();
+            } else if (gamewin) {
+                ShowGameWinScreen();
+            }
         }
     }
 };
 
-
 int main() {
-    Game snakeGame;
-    snakeGame.Run();
+    SnakeGame game;
+    game.Run();
     return 0;
 }
